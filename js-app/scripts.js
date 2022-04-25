9d09c615f8190816187fed776dfc064162cca577
Ivan <ivan.frontcoder@gmail.com>
2022-04-25 19:56:41 +0500

    .

--- js-app/scripts.js
+++ js-app/scripts.js
@@ -143,38 +143,38 @@ $(document).ready(function () {
 	// }
 	// tabs($('.js-tabs'));
 
-	// // Аккордеон
-	// function accordion() {
-	// 	if ($('.accordion').length) {
-	// 		$('.accordion').each(function () {
-	// 			var accordion = $(this),
-	// 				trigger = accordion.find('.accordion__trigger'),
-	// 				time = 300;
-	// 			trigger.on('click', function () {
-	// 				var $thisTrigger = $(this),
-	// 					data = $thisTrigger.data('trigger');
-	// 				if (!$thisTrigger.hasClass('active')) {
-	// 					$thisTrigger.addClass('active');
-	// 					accordion.find('#' + data).stop().slideDown(
-	// 						time,
-	// 						function () {
-	// 							$(this).addClass('open')
-	// 						}
-	// 					);
-	// 				} else {
-	// 					$thisTrigger.removeClass('active');
-	// 					accordion.find('#' + data).stop().slideUp(
-	// 						time,
-	// 						function () {
-	// 							$(this).removeClass('open')
-	// 						}
-	// 					);
-	// 				}
-	// 			})
-	// 		})
-	// 	}
-	// }
-	// accordion();
+	// Аккордеон
+	function accordion() {
+		if ($('.accordion').length) {
+			$('.accordion').each(function () {
+				var accordion = $(this),
+					trigger = accordion.find('.accordion__trigger'),
+					time = 300;
+				trigger.on('click', function () {
+					var $thisTrigger = $(this),
+						data = $thisTrigger.data('trigger');
+					if (!$thisTrigger.hasClass('active')) {
+						$thisTrigger.addClass('active');
+						accordion.find('#' + data).stop().slideDown(
+							time,
+							function () {
+								$(this).addClass('open')
+							}
+						);
+					} else {
+						$thisTrigger.removeClass('active');
+						accordion.find('#' + data).stop().slideUp(
+							time,
+							function () {
+								$(this).removeClass('open')
+							}
+						);
+					}
+				})
+			})
+		}
+	}
+	accordion();
 
 	// // Модальное окно
 	// function modal(modal) {
@@ -906,6 +906,11 @@ $(document).ready(function () {
 				spaceBetween: 16
 			}
 		}
+	});
+
+	// FormStyler
+	$('select').styler({
+		selectVisibleOptions: 10,
 	});
 
 });
